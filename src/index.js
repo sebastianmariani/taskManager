a66452bebd352f2fedc908e8cf442347091f89b1
User <sebastian.mariani.94@gmail.com>
2020-12-29 15:04:26 +0000

    added image avatar

--- src/index.js
+++ src/index.js
@@ -6,27 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word document'))
-        }
-
-        cb(null, true)
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -34,18 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5fe084b38b393084da5171d2')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    // const user = await User.findById('5fe084a88b393084da5171d0')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-}
-
-main()
