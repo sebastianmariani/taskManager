895e9e432434db406f45b51dbbd954283e03ed0f
User <sebastian.mariani.94@gmail.com>
2021-05-07 16:30:45 +0100

    test conversion fahreneheit to celcius

--- src/math.js
+++ src/math.js
@@ -1,8 +1,16 @@
-const calculateTip = (total, tipPercent) => {
-    const tip = total * tipPercent
-    return total + tip
+const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent)
+
+
+const fahrenheitToCelsius = (temp) => {
+    return (temp - 32) / 1.8
+}
+
+const celciusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32
 }
 
 module.exports = {
-    calculateTip
+    calculateTip,
+    fahrenheitToCelsius,
+    celciusToFahrenheit
 }
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,10 +1,21 @@
-const { calculateTip } = require("../src/math");
+const { calculateTip, fahrenheitToCelsius, celciusToFahrenheit } = require("../src/math");
 
 test('should calculare total with tip', () => {
     const total = calculateTip(10, .3)
-
-    if (total !== 13) {
-        throw new Error('Total tip should be 13. Got ' + total)
-    }
+    expect(total).toBe(13)
 })
 
+test('should calculate total with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
+})
+
+test('should convert 32 fahrenheit to 0 celcius', () => {
+    const conversion = fahrenheitToCelsius(32)
+    expect(conversion).toBe(0)
+})
+
+test('Should convert celcius to farhenheit', () => {
+    const conversion = celciusToFahrenheit(0)
+    expect(conversion).toBe(32)
+})
